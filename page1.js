bd43de91376cd49312c544e68534a5bb487c88e5
beatrixdaa <beatrixdevanti21@gmail.com>
2023-06-19 11:15:02 +0700

    add script

---
+++ page1.js
@@ -0,0 +1,9 @@
+video.addEventListener("click", function video(){
+    alert("Teaser Audition Indonesian Idol 2024");
+});
+audio.addEventListener("click", function audio(){
+    alert("Indonesian Idol 2024");
+});
+gambar.addEventListener("click", function gambar(){
+    alert("Indonesian Idol 2024");
+});
